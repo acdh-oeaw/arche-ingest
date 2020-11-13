3ed1478c1c7908a9177af5fb84ac53aaf077ad5e
Mateusz Żółtak <mzoltak@oeaw.ac.at>
2020-11-13 12:00:02 +0100

    delete_metadata_sample.php now resolves URI objects instead of treating them literally

--- delete_metadata_sample.php
+++ delete_metadata_sample.php
@@ -40,6 +40,9 @@ foreach ($graph->resources() as $r) {
                     if ($v instanceof \EasyRdf\Literal) {
                         $dtLang = !empty($v->getLang()) ? '@' . $v->getLang() : '';
                         $dtLang .= !empty($v->getDatatype()) ? '^^' . $v->getDatatype() : '';
+                    } else {
+                        $dr = $repo->getResourceById($v->getUri());
+                        $v = $meta->getGraph()->resource($dr->getUri());
                     }
                     echo "\tremoving $p " . (string) $v . $dtLang . "\n";
                     $meta->delete($p, $v);
