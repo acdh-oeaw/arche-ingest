2abeb0102bcc6296332792620de718bd59dd23b6
Mateusz Żółtak <zozlak@zozlak.org>
2022-11-24 16:23:13 +0100

    update_redmine.php: add --append and cli parameter help texts

--- update_redmine.php
+++ update_redmine.php
@@ -34,18 +34,19 @@ $subtasks = [
     ],
 ];
 
-$parser = new AP();
-$parser->addArgument('--user');
-$parser->addArgument('--pswd');
-$parser->addArgument('--token');
-$parser->addArgument('--message');
+$parser = new AP('Updates information on ingestion progress in the Redmine.');
+$parser->addArgument('--user', help: "Redmine user.");
+$parser->addArgument('--pswd', help: "Redmine user's password");
+$parser->addArgument('--token', help: "Redmine authorization token. Can be used instead of --user and --pswd.");
+$parser->addArgument('--message', help: "Message to be stored as a Redmine issue note. If not provided, a default message is used. There are different default messages for each subtask and successful/unsuccessful status code.");
+$parser->addArgument('--append', help: "Message to be appended to the main one. Useful for combining the default message with a custom one.");
 $parser->addArgument('--status', choices: ['New', 'In Progress', 'Resolved', 'Feedback',
-    'Closed', 'Rejected', 'Needs review', 'On Hold']);
-$parser->addArgument('--done', type: AP::TYPE_INT);
-$parser->addArgument('--statusCode', type: AP::TYPE_INT, default: 0);
-$parser->addArgument('redmineApiUrl');
-$parser->addArgument('mainIssueId'); // use 21085 for testing
-$parser->addArgument('subtask', choices: array_keys($subtasks));
+    'Closed', 'Rejected', 'Needs review', 'On Hold'], help: "Set Redmine issue status to a given value. If not provided, the current status is preserved.");
+$parser->addArgument('--done', type: AP::TYPE_INT, help: "Set Redmine issue 'done' field to a given value (from 0 to 100). If not provided, the current field value is kept.");
+$parser->addArgument('--statusCode', type: AP::TYPE_INT, default: 0, help: "Indicates if a successful (--statusCode 0) on unsuccessful (any other value) variant of a message should be used as the Redmine issue note. If not provided, the successful variant is used. Has no impact if --message parameter is used.");
+$parser->addArgument('--redmineApiUrl', default: 'https://redmine.acdh.oeaw.ac.at', help: "Redmine API base URL");
+$parser->addArgument('mainIssueId', help: "Redmine issue ID of a main collection ingestion tracking issue, e.g. https://redmine.acdh.oeaw.ac.at/issues/21016"); // use 21085 for testing
+$parser->addArgument('subtask', choices: array_keys($subtasks), help: "Ingestion subtask to note the information about");
 $args   = $parser->parseArgs();
 
 if ((empty($args->user) || empty($args->pswd)) && empty($args->token)) {
@@ -97,12 +98,11 @@ if (!empty($args->done)) {
 }
 
 $msg = $args->message ?? $subtasks[$args->subtask][$args->statusCode === 0 ? 0 : 1];
-$issuesApi->addNoteToIssue($issue, $msg);
-if (!empty(getenv('GITHUB_RUN_ID'))) {
-    $msg = rtrim($msg) . "\n\n" . getenv('GITHUB_SERVER_URL') . '/' . getenv('GITHUB_REPOSITORY') . '/actions/runs/' . getenv('GITHUB_RUN_ID');
+if (!empty($args->url)) {
+    $msg = rtrim($msg) . "\n\n$url";
 }
+$issuesApi->addNoteToIssue($issue, $msg);
 
 echo "Redmine issue $issue updated successfully\n";
-print_r($_ENV);
 exit(0);
 
